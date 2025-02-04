1eb876e6bbfcc535e913dfd832b37bdc76d5466c
coder-hiro <hiro.09921@gmail.com>
2025-02-04 23:51:29 +0900

    誕生日企画

---
+++ Vegas2/js/script.js
@@ -0,0 +1,13 @@
+$(".mvInner").vegas({
+    slides: [
+        { src: "./img/zoom01.webp" },
+        { src: "./img/zoom02.webp" },
+        { src: "./img/zoom03.webp" },
+        { src: "./img/zoom04.webp" },
+        { src: "./img/zoom05.webp" },
+        { src: "./img/zoom06.webp" },
+        { src: "./img/zoom07.webp" },
+        { src: "./img/zoom08.webp" },
+        { src: "./img/zoom09.webp" },
+    ]
+});
