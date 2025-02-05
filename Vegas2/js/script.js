3ee25afaef7d60b560fedf0754788239ab5c4bfa
coder-hiro <hiro.09921@gmail.com>
2025-02-05 23:24:09 +0900

    0205

--- Vegas2/js/script.js
+++ Vegas2/js/script.js
@@ -11,3 +11,20 @@ $(".mvInner").vegas({
         { src: "./img/zoom09.webp" },
     ]
 });
+
+if(window.matchMedia('(max-width: 767px)').matches) {
+    $(".mvInner").vegas({
+        slides: [
+            { src: "./img/sp/zoom01_sp.webp" },
+            { src: "./img/sp/zoom02_sp.webp" },
+            { src: "./img/sp/zoom03_sp.webp" },
+            { src: "./img/sp/zoom04_sp.webp" },
+            { src: "./img/sp/zoom05_sp.webp" },
+            { src: "./img/sp/zoom06_sp.webp" },
+            { src: "./img/sp/zoom07_sp.webp" },
+            { src: "./img/sp/zoom08_sp.webp" },
+            { src: "./img/sp/zoom09_sp.webp" },
+
+        ]
+    });
+}
