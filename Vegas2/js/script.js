ad4e32627d91c27d25078d81568817ea8fe99a8a
coder-hiro <hiro.09921@gmail.com>
2025-02-06 21:17:30 +0900

    0206

--- Vegas2/js/script.js
+++ Vegas2/js/script.js
@@ -1,42 +1,37 @@
-$(document).ready(function() {
-    function initializeVegas() {
-        if(window.matchMedia('(max-width: 767px)').matches) {
-            $(".mvInner").vegas({
-                slides: [
-                    { src: "./img/sp/zoom01_sp.webp" },
-                    { src: "./img/sp/zoom02_sp.webp" },
-                    { src: "./img/sp/zoom03_sp.webp" },
-                    { src: "./img/sp/zoom04_sp.webp" },
-                    { src: "./img/sp/zoom05_sp.webp" },
-                    { src: "./img/sp/zoom06_sp.webp" },
-                    { src: "./img/sp/zoom07_sp.webp" },
-                    { src: "./img/sp/zoom08_sp.webp" },
-                    { src: "./img/sp/zoom09_sp.webp" },
-                ]
-            });
-        } else {
-            $(".mvInner").vegas({
-                slides: [
-                    { src: "./img/zoom01.webp" },
-                    { src: "./img/zoom02.webp" },
-                    { src: "./img/zoom03.webp" },
-                    { src: "./img/zoom04.webp" },
-                    { src: "./img/zoom05.webp" },
-                    { src: "./img/zoom06.webp" },
-                    { src: "./img/zoom07.webp" },
-                    { src: "./img/zoom08.webp" },
-                    { src: "./img/zoom09.webp" },
-                ]
-            });
-        }
+$(document).ready(function () {
+  function initializeVegas() {
+    if (window.matchMedia("(max-width: 767px)").matches) {
+      $(".mvInner").vegas({
+        slides: [
+          { src: "./img/sp/new01_sp.webp" },
+          { src: "./img/sp/new02_sp.webp" },
+          { src: "./img/sp/new03_sp.webp" },
+          { src: "./img/sp/new04_sp.webp" },
+        ],
+      });
+    } else {
+      $(".mvInner").vegas({
+        slides: [
+          { src: "./img/zoom01.webp" },
+          { src: "./img/zoom02.webp" },
+          { src: "./img/zoom03.webp" },
+          { src: "./img/zoom04.webp" },
+          { src: "./img/zoom05.webp" },
+          { src: "./img/zoom06.webp" },
+          { src: "./img/zoom07.webp" },
+          { src: "./img/zoom08.webp" },
+          { src: "./img/zoom09.webp" },
+        ],
+      });
     }
+  }
 
-    // 初期化
+  // 初期化
+  initializeVegas();
+
+  // ウィンドウのリサイズイベントをリッスン
+  $(window).resize(function () {
+    $(".mvInner").vegas("destroy");
     initializeVegas();
-
-    // ウィンドウのリサイズイベントをリッスン
-    $(window).resize(function() {
-        $(".mvInner").vegas('destroy');
-        initializeVegas();
-    });
+  });
 });
